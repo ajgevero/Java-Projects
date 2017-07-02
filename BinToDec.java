8afe4c0dee0e267ab784afeca26650c4b197dc26
ajgevero <adrianjamesgevero@gmail.com>
2017-07-02 17:37:35 -0400

    Update BinToDec.java

--- BinToDec.java
+++ BinToDec.java
@@ -1,44 +1,48 @@
 import java.util.Scanner;
-
 public class BinToDec {
 	private static int arry[] = new int[8];
 	
 	public static void main(String[] args) {
-		takeBin();
-		if (checkNumber() == true) {
-			System.out.println(Construct());
+		takeBin();			// Go to takeBin method
+		if (checkNumber() == true) {						// Conditional checks checkNumber method
+			System.out.println("\n" + Construct());
 		}
 		else{
 			System.out.println("Sorry, the number you've entered is invalid.");
 		}
+		main(args);
 	}
 	
 	public static void takeBin(){
-		System.out.println("Enter 8 bits of your binary number: ");
-		int dec = new Scanner(System.in).nextInt();
-		for (int a = 7; a >= 0; a--){
+		System.out.println("Enter 8 bits of your binary number: ");		// Prompt
+		int dec = extracted().nextInt();			//receives user input (8-bit decimal)
+		for (int a = 7; a >= 0; a--){		// For loop de-constructs the input into its digits and places in an array
 			arry[a] = (dec % 10);
 			dec /= 10;
 		}
 	}
+
+	private static Scanner extracted() {
+		return new Scanner(System.in);			// Closes scanner
+	}
 	
-	public static boolean checkNumber(){
-		boolean check = false;
+	public static boolean checkNumber(){			
+		boolean check = false;		// Boolean value instantiated as initially false
 		for (int i : arry){
 			if (i == 0 || i == 1){
-				check = true;
+				check = true;		// if there is a 1 or 0 digit, the Boolean check is changed to true
 			}
 			else{
-				return false;
+				return false;		// if there is any number that is not 1 or 0 the boolean is instantly returned false
 			}
 		}
 		return check;
 	}
 	
 	public static int Construct() {
-		int e = 7;
+		int e = 7;		// exponent value
 		int temp = 0;
-		for (int i : arry){
+		for (int i : arry){		//incrementally adds the value of the bit if its binary value is 1
 			if (i == 1){
 				temp += Math.pow(2, e);
 				e--;
@@ -47,8 +51,6 @@ public class BinToDec {
 				e--;
 			}
 		}
-		System.out.println("Here is your number in Base-10 Decimal: ");
 		return temp;
 	}	
 }
-	
